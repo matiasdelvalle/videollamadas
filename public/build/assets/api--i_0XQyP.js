7a7e763dbbee0e4505e692cc16b7e9180d73fa94
Matias <macbook@MacBook-Pro.local>
2026-04-04 18:32:51 -0300

    build

---
+++ public/build/assets/api--i_0XQyP.js
@@ -0,0 +1,1 @@
+import{E as a}from"./app-DPGi9S-Y.js";const e="/".replace(/\/+$/,""),p=a.create({baseURL:`${e}`});export{p as a};
--- public/build/assets/api-B4i4Bsj9.js
+++
@@ -1,1 +0,0 @@
-import{E as a}from"./app-Pngyr5nY.js";const e="/videollamada/public/".replace(/\/+$/,""),p=a.create({baseURL:`${e}`});export{p as a};
