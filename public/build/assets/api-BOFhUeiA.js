9c366ebb0bca64f20936f0d402d66629dbff34cb
Matias <macbook@MacBook-Pro.local>
2026-09-10 14:53:04 -0300

    build

---
+++ public/build/assets/api-BOFhUeiA.js
@@ -0,0 +1,1 @@
+import{H as a}from"./app-Craizb7D.js";const e="/gms/videollamada/public/".replace(/\/+$/,""),p=a.create({baseURL:`${e}`});export{p as a};
--- resources/js/lib/api.js
+++ resources/js/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE = (import.meta.env.VITE_BASE_URL || '/').replace(/\/+$/, '') // sin barra final
+const BASE = (import.meta.env.VITE_BASE_API || '/').replace(/\/+$/, '') // sin barra final
 export const api = axios.create({
   baseURL: `${BASE}`,
 })
