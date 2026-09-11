965de935d33ea610c3f4296d1c2a38efc482add3
Matias <macbook@MacBook-Pro.local>
2026-09-11 09:46:16 -0300

    build

---
+++ public/build/assets/api-Ba9kuKkx.js
@@ -0,0 +1,1 @@
+import{H as a}from"./app-3M6NWlLc.js";const e="/".replace(/\/+$/,""),p=a.create({baseURL:`${e}`});export{p as a};
--- public/build/assets/api-CZiu1yRU.js
+++
@@ -1,1 +0,0 @@
-import{H as a}from"./app-BMr0qs2m.js";const e="/".replace(/\/+$/,""),p=a.create({baseURL:`${e}`});export{p as a};
